49d93655264a9650383382b8a891b9359c60df56
lbarret4 <llewellynbar4@gmail.com>
2018-09-30 12:59:14 -0400

    added authentification route,  isLoggedin and tokenmiddleware  to routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,8 +2,18 @@ import { Router } from 'express';
 import blogsRouter from './blogs';
 import authorsRouter from "./authors";
 import tagsRouter from "./tags";
+import authRouter from './auth';
+import usersRouter from './users';
+import {tokenMiddleware, isLoggedIn} from '../middleware/auth.mw';
 
 let router = Router();
+
+
+router.use('/auth',authRouter);
+router.use(tokenMiddleware);
+router.use(isLoggedIn);
+
+router.use(usersRouter);
 
 router.use('/blogs', blogsRouter);
 router.use('/authors',authorsRouter);
