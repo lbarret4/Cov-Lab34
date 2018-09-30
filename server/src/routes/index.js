e7ad9ed1bfa0dcea37075c98d8c5aedc253a1b27
lbarret4 <llewellynbar4@gmail.com>
2018-09-30 03:05:15 -0400

    blog setup and api files

---
+++ server/src/routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import blogsRouter from './blogs';
+import authorsRouter from "./authors";
+import tagsRouter from "./tags";
+
+let router = Router();
+
+router.use('/blogs', blogsRouter);
+router.use('/authors',authorsRouter);
+router.use('/tags',tagsRouter);
+
+
+export default router;
